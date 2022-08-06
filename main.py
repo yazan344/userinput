30ef31cae583248e1b38f8da7465ffd39c309616
yazan <yazanadel07@gmail.com>
2022-08-06 18:26:39 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+name = input("What is your name :")
+age = int(input("Your age :"))
+height = int(input("What is your height"))
+weight = int(input("What is weight")
+
+print ("Hi "+name)
+print ("You are "+str(age)+"years old")
+print ("You are "+str(height)+"cm tall")
+print ("You are "+str(weight)+"kg your weight")
